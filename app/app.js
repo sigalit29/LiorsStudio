87d988b892b61b4c87fadea1eb9faac60d369cda
Sigalit <siga.m.sm@gmail.com>
2019-06-24 20:33:35 +0300

    Adding an image background to landing page

--- app/app.js
+++ app/app.js
@@ -1,2 +1,17 @@
 
-var app = angular.module("yogaStudio", []);
+var app = angular.module("yogaStudio", ["ngRoute"]);
+
+app.config(function($routeProvider) {
+    $routeProvider
+    .when("/", {
+        templateUrl: "app/home/home.html"
+    }).when("/login", {
+
+    }).when("/signup", {
+
+    }).when("/recipes", {
+
+    }).when("/new", {
+
+    })
+}) 
