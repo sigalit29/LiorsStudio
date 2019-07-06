6fb1158157e29b74783e12fd455abb407abf3d56
Sigalit <siga.m.sm@gmail.com>
2019-07-06 13:38:24 +0300

    Add modal for user weekly text updates

--- app/weeklyActivity/weeklyActivityCtrl.js
+++ app/weeklyActivity/weeklyActivityCtrl.js
@@ -1,5 +1,21 @@
-app.controller("weeklyActivityCtrl", function($scope, $location, $log) {
+app.controller("weeklyActivityCtrl", function ($scope, $uibModal) {
+
+    $scope.userText ="";
+    $scope.openUpdateText = function () {
+        var modalInstance = $uibModal.open({
+            templateUrl: "app/weeklyActivity/weeklyActivityModal.html",
+            controller: "weeklyActivityModalCtrl"
+        })
+
+        modalInstance.result.then(function (userText) {
+            // this will wake in case the user added a new recipe
+            $scope.userText = userText;
+           
+        }, function () {
+            // this will wake up in case the user canceled the new recipe
+            console.log("user canceled text update");
+        })
+    };
 
 
- 
-})
+});
---
+++ app/weeklyActivity/weeklyActivityModalCtrl.js
@@ -0,0 +1,16 @@
+app.controller("weeklyActivityModalCtrl", function ($scope, $uibModalInstance) {
+
+    $scope.animationsEnabled = true;
+    $scope.weeklyText = "";
+
+    $scope.ok = function () {
+        $uibModalInstance.close($scope.weeklyText);
+    };
+
+    $scope.cancel = function () {
+        $uibModalInstance.dismiss();
+    };
+
+
+
+});
